18c0a92724127ee1a31c0d1a495f2437b961a77f
Tremho <steve@ohmert.com>
2020-10-20 11:37:38 -0700

    Yeti persists

--- ThunderBolt/electronMain/make-releases.sh
+++ ThunderBolt/electronMain/make-releases.sh
@@ -2,19 +2,20 @@ wpk=`which electron-packager`
 wwn=`which wine64`
 plt=`uname`
 
-if [[ "$wpk" == "" ]]; then
+if "$wpk"=""; then
   npm install -g electron-packager
 fi
 
 # Announce for debugging trail if needed
 electron-packager --version
 
+rm -fr release-builds
+
 # make Mac
 electron-packager . --overwrite --platform=darwin --arch=x64 --extraResource=../index.html --extraResource=../css --extraResource=../dist --icon=assets/icons/mac/icon.icns --prune=true --out=release-builds
 
 # make Windows
-echo "Making mac package"
-if [[ "$wwn" == "" ]]; then
+if "$wwn"=""; then
   echo "Not creating Windows app -- wine64 is not available. Daddy Needs Wine!"
 else
   electron-packager . HumanTest --overwrite --asar --platform=win32 --arch=ia32  --extraResource=../index.html --extraResource=../css --extraResource=../dist  --icon=assets/icons/win/icon.ico --prune=true --out=release-builds --version-string.CompanyName=CE --version-string.FileDescription=CE --version-string.ProductName="HumanTest"
