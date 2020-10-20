7aa2ef0b7c2e0f6baeb70e898680ac3703cd3858
Tremho <steve@ohmert.com>
2020-10-20 11:57:16 -0700

    Bash the Yeti

--- ThunderBolt/electronMain/make-releases.sh
+++ ThunderBolt/electronMain/make-releases.sh
@@ -1,9 +1,15 @@
 wpk=`which electron-packager`
 wwn=`which wine64`
+wdc=`which documentation`
 plt=`uname`
 
 if [ $wpk == ""]; then
   npm install -g electron-packager
+fi
+
+# we'll need this later on, may as well do it here
+if [ $wdc == ""]; then
+  npm install -g documentation
 fi
 
 # Announce for debugging trail if needed
@@ -15,10 +21,10 @@ rm -fr release-builds
 electron-packager . --overwrite --platform=darwin --arch=x64 --extraResource=../index.html --extraResource=../css --extraResource=../dist --icon=assets/icons/mac/icon.icns --prune=true --out=release-builds
 
 # make Windows
-if [ $wwn == "" ]; then
-  echo "Not creating Windows app -- wine64 is not available. Daddy Needs Wine!"
-else
+if [ $wwn != "" ]; then
   electron-packager . HumanTest --overwrite --asar --platform=win32 --arch=ia32  --extraResource=../index.html --extraResource=../css --extraResource=../dist  --icon=assets/icons/win/icon.ico --prune=true --out=release-builds --version-string.CompanyName=CE --version-string.FileDescription=CE --version-string.ProductName="HumanTest"
+else
+  echo "Not creating Windows app -- wine64 is not available. Daddy Needs Wine!"
 fi
 
 # make Linux
