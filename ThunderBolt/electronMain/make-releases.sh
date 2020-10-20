5a8dc8e23dd1a7a06f9ef18fc22ceab0394eea6a
Tremho <steve@ohmert.com>
2020-10-20 11:14:24 -0700

    Discovery conditionals for make-releases.sh

--- ThunderBolt/electronMain/make-releases.sh
+++ ThunderBolt/electronMain/make-releases.sh
@@ -1,11 +1,22 @@
+wpk=`which electron-packager`
+wwn=`which wine64`
+plt=`uname`
 
-npm install -g electron-packager
+if [ "$wpk" == "" ]; then
+  npm install -g electron-packager
+fi
 
 # make Mac
 electron-packager . --overwrite --platform=darwin --arch=x64 --extraResource=../index.html --extraResource=../css --extraResource=../dist --icon=assets/icons/mac/icon.icns --prune=true --out=release-builds
 
 # make Windows
-electron-packager . HumanTest --overwrite --asar --platform=win32 --arch=ia32  --extraResource=../index.html --extraResource=../css --extraResource=../dist  --icon=assets/icons/win/icon.ico --prune=true --out=release-builds --version-string.CompanyName=CE --version-string.FileDescription=CE --version-string.ProductName="HumanTest"
+echo "Making mac package"
+if [ "$wwn" == "" ]; then
+  echo "Not creating Windows app -- wine64 is not available"
+else
+  electron-packager . HumanTest --overwrite --asar --platform=win32 --arch=ia32  --extraResource=../index.html --extraResource=../css --extraResource=../dist  --icon=assets/icons/win/icon.ico --prune=true --out=release-builds --version-string.CompanyName=CE --version-string.FileDescription=CE --version-string.ProductName="HumanTest"
+fi
+
 
 # make Linux
 electron-packager . HumanTest --overwrite --asar --platform=linux --arch=x64 --extraResource=../index.html --extraResource=../css --extraResource=../dist   --icon=assets/icons/png/1024x1024.png --prune=true --out=release-builds
