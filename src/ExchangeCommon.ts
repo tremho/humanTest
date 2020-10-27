eadeacd53c1b8686a63c55530f24b893d3783ab0
Tremho <steve@ohmert.com>
2020-10-27 16:27:14 -0700

    test name introduced in v0.3.0 (Test Report)

--- src/ExchangeCommon.ts
+++ src/ExchangeCommon.ts
@@ -21,17 +21,21 @@ export class TestResponse {
  * Options that *may* be passed to each command.
  * If not given, defaults for each value are used.
  *
+ * _Note: in v0.3.0 the default test timeout was changed from 30 to 60_
+ *
  * | Property |  Type | Default | Purpose |
  * | :------- | :---  | :------ | :-----  |
  * | prompt    | string | "Is this acceptable?" | The prompt to display to the user
  * | specialNotice | string | <none> | A message displayed in a modal alert dialog to gove the user special instruction
  * | title | string | previous | (since v0.3.0) Changes the main title.  The title is initially set with startManualTest, and can be changed with any command. The title will persist beyond the life of the command, until changed again.|
- * | timeout  | number | 30 (120 for verifyHuman) | The number of seconds before a timeout occurs, and test skipped
+ * | name | string | derived from command | (since v0.3.0) assigns the test naem that the results appear under in the HumanTest Report output
+ * | timeout  | number | 60 (120 for verifyHuman) | The number of seconds before a timeout occurs, and test skipped
  * | width | number | <none> | **(`imageView` Only)** Defines the width used for the image display, in pixels. If not provided, the full image width is displayed. |
  * | height | number | <none> | **(`imageView` Only)** Defines the height used for the image display, in pixels. If not provided, the full image height is displayed. |
  *
  */
 export class TestOptions {
+    public name?: string
     public prompt?: string
     public timeout?: number
     public specialNotice?: string
